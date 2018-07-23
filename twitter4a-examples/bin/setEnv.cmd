18f35cf4bf362867a405376d831cb04badcbd9bf
takke <hiroaki@v7.com>
2018-07-24 00:48:22 +0900

    add CLASSPATH

--- twitter4a-examples/bin/setEnv.cmd
+++ twitter4a-examples/bin/setEnv.cmd
@@ -2,10 +2,9 @@ rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_121\jre
 
 for %%i in (..\lib\*.jar) do set CLASSPATH=!CLASSPATH!;%%i
 
-
-rem set CLASSPATH=!CLASSPATH!;twitter4j-examples-4.1.0-beta4.jar
-set CLASSPATH=%CLASSPATH%;D:\Src\twitter4j_work\twitter4j\twitter4j-examples\build\libs\twitter4j-examples-4.1.0-beta4.jar
-set CLASSPATH=%CLASSPATH%;D:\Src\twitter4j_work\twitter4j\twitter4j-core\build\libs\twitter4j-core-4.1.0-beta4.jar
-set CLASSPATH=%CLASSPATH%;D:\Src\twitter4j_work\twitter4j\twitter4j-stream\build\libs\twitter4j-stream-4.1.0-beta4.jar
+set T4A_VER=4.1.0-beta5
+set CLASSPATH=%CLASSPATH%;..\..\twitter4a-examples\build\libs\twitter4a-examples-%T4A_VER%.jar
+set CLASSPATH=%CLASSPATH%;..\..\twitter4a-core\build\libs\twitter4a-core-%T4A_VER%.jar
+set CLASSPATH=%CLASSPATH%;..\..\twitter4a-stream\build\libs\twitter4a-stream-%T4A_VER%.jar
 
 set MEM_ARGS=-Xms30m -Xmx30m
