c3954c8108a8b14fd10e13c47270824b12026543
takke <takke30@gmail.com>
2023-12-01 13:30:57 +0900

    add card

--- twitter4j-core/src/internal-async/java/twitter4j/Card.kt
+++ twitter4j-core/src/internal-async/java/twitter4j/Card.kt
@@ -1,5 +1,12 @@
 package twitter4j
 
-class Card {
+data class Card(
+    val url: String,
 
-}
+    // header image url : the largest image of multiple images
+    val imageUrl: String?,
+    val imageWidth: Int,
+    val imageHeight: Int,
+
+    val title: String,
+)
