0990f7f083a18478e937dc64e3fc566550c19b2a
takke <takke30@gmail.com>
2024-01-23 18:59:31 +0900

    Paging.kt

--- twitter4j-core/src/main/java/twitter4j/Paging.kt
+++ twitter4j-core/src/main/java/twitter4j/Paging.kt
@@ -13,61 +13,65 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package twitter4j
 
-package twitter4j;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable
 
 /**
- * Controls pagination.<br>
+ * Controls pagination.<br></br>
  * It is possible to use the same Paging instance in a multi-threaded
- * context only if the instance is treated immutably.<br>
+ * context only if the instance is treated immutably.<br></br>
  * But basically instance of this class is NOT thread safe.
- * A client should instantiate Paging class per thread.<br>
+ * A client should instantiate Paging class per thread.<br></br>
  *
  * @author Yusuke Yamamoto - yusuke at mac.com
  */
-public final class Paging implements java.io.Serializable {
-    private static final long serialVersionUID = -7226113618341047983L;
-    private int page = -1;
-    private int count = -1;
-    private long sinceId = -1;
-    private long maxId = -1;
-
-    // since only
-    static final char[] S = new char[]{'s'};
-    // since, max_id, count, page
-    static final char[] SMCP = new char[]{'s', 'm', 'c', 'p'};
-
-    static final String COUNT = "count";
-    // somewhat GET list statuses requires "per_page" instead of "count"
-    // @see <a href="https://dev.twitter.com/docs/api/1.1/get/:user/lists/:id/statuses">GET :user/lists/:id/statuses | Twitter Developers</a>
-    static final String PER_PAGE = "per_page";
-
-    /*package*/ List<HttpParameter> asPostParameterList() {
-        return asPostParameterList(SMCP, COUNT);
-    }
-
-    private static final HttpParameter[] NULL_PARAMETER_ARRAY = new HttpParameter[0];
-
-    /*package*/ HttpParameter[] asPostParameterArray() {
-        List<HttpParameter> list = asPostParameterList(SMCP, COUNT);
-        if (list.size() == 0) {
-            return NULL_PARAMETER_ARRAY;
+class Paging : Serializable {
+    private var _page = -1
+    var page: Int
+        get() = _page
+        set(value) {
+            require(value >= 1) { "page should be positive integer. passed:$_page" }
+            _page = value
         }
-        return list.toArray(new HttpParameter[list.size()]);
+
+    private var _count: Int = -1
+    var count: Int
+        get() = _count
+        set(value) {
+            require(value >= 1) { "count should be positive integer. passed:$_count" }
+
+            _count = value
+        }
+
+    private var _sinceId: Long = -1
+    var sinceId: Long
+        get() = _sinceId
+        set(value) {
+            require(value >= 1) { "since_id should be positive integer. passed:$_sinceId" }
+
+            _sinceId = value
+        }
+
+    private var _maxId: Long = -1
+    var maxId: Long
+        get() = _maxId
+        set(value) {
+            require(value >= 1) { "max_id should be positive integer. passed:$_maxId" }
+
+            _maxId = value
+        }
+
+    /*package*/
+    fun asPostParameterArray(): Array<HttpParameter?> {
+        val list = asPostParameterList(SMCP, COUNT)
+        return if (list.size == 0) {
+            NULL_PARAMETER_ARRAY
+        } else list.toTypedArray<HttpParameter?>()
     }
-
-    /*package*/ List<HttpParameter> asPostParameterList(char[] supportedParams) {
-        return asPostParameterList(supportedParams, COUNT);
-    }
-
-
-    private static final List<HttpParameter> NULL_PARAMETER_LIST = new ArrayList<HttpParameter>(0);
 
     /**
-     * Converts the pagination parameters into a List of PostParameter.<br>
+     * Converts the pagination parameters into a List of PostParameter.<br></br>
      * This method also Validates the preset parameters, and throws
      * IllegalStateException if any unsupported parameter is set.
      *
@@ -75,21 +79,23 @@ public final class Paging implements java.io.Serializable {
      * @param perPageParamName name used for per-page parameter. getUserListStatuses() requires "per_page" instead of "count".
      * @return list of PostParameter
      */
-    /*package*/ List<HttpParameter> asPostParameterList(char[] supportedParams, String perPageParamName) {
-        java.util.List<HttpParameter> pagingParams = new ArrayList<HttpParameter>(supportedParams.length);
-        addPostParameter(supportedParams, 's', pagingParams, "since_id", getSinceId());
-        addPostParameter(supportedParams, 'm', pagingParams, "max_id", getMaxId());
-        addPostParameter(supportedParams, 'c', pagingParams, perPageParamName, getCount());
-        addPostParameter(supportedParams, 'p', pagingParams, "page", getPage());
-        if (pagingParams.size() == 0) {
-            return NULL_PARAMETER_LIST;
+    /*package*/ /*package*/ /*package*/
+    @JvmOverloads
+    fun asPostParameterList(supportedParams: CharArray = SMCP, perPageParamName: String = COUNT): List<HttpParameter?> {
+        val pagingParams: MutableList<HttpParameter?> = ArrayList(supportedParams.size)
+        addPostParameter(supportedParams, 's', pagingParams, "since_id", sinceId)
+        addPostParameter(supportedParams, 'm', pagingParams, "max_id", maxId)
+        addPostParameter(supportedParams, 'c', pagingParams, perPageParamName, count.toLong())
+        addPostParameter(supportedParams, 'p', pagingParams, "page", page.toLong())
+        return if (pagingParams.size == 0) {
+            NULL_PARAMETER_LIST
         } else {
-            return pagingParams;
+            pagingParams
         }
     }
 
     /**
-     * Converts the pagination parameters into a List of PostParameter.<br>
+     * Converts the pagination parameters into a List of PostParameter.<br></br>
      * This method also Validates the preset parameters, and throws
      * IllegalStateException if any unsupported parameter is set.
      *
@@ -97,158 +103,108 @@ public final class Paging implements java.io.Serializable {
      * @param perPageParamName name used for per-page parameter. getUserListStatuses() requires "per_page" instead of "count".
      * @return list of PostParameter
      */
-    /*package*/ HttpParameter[] asPostParameterArray(char[] supportedParams, String perPageParamName) {
-        java.util.List<HttpParameter> pagingParams = new ArrayList<HttpParameter>(supportedParams.length);
-        addPostParameter(supportedParams, 's', pagingParams, "since_id", getSinceId());
-        addPostParameter(supportedParams, 'm', pagingParams, "max_id", getMaxId());
-        addPostParameter(supportedParams, 'c', pagingParams, perPageParamName, getCount());
-        addPostParameter(supportedParams, 'p', pagingParams, "page", getPage());
-        if (pagingParams.size() == 0) {
-            return NULL_PARAMETER_ARRAY;
+    /*package*/
+    fun asPostParameterArray(supportedParams: CharArray, perPageParamName: String): Array<HttpParameter?> {
+        val pagingParams: MutableList<HttpParameter?> = ArrayList(supportedParams.size)
+        addPostParameter(supportedParams, 's', pagingParams, "since_id", sinceId)
+        addPostParameter(supportedParams, 'm', pagingParams, "max_id", maxId)
+        addPostParameter(supportedParams, 'c', pagingParams, perPageParamName, count.toLong())
+        addPostParameter(supportedParams, 'p', pagingParams, "page", page.toLong())
+        return if (pagingParams.size == 0) {
+            NULL_PARAMETER_ARRAY
         } else {
-            return pagingParams.toArray(new HttpParameter[pagingParams.size()]);
+            pagingParams.toTypedArray<HttpParameter?>()
         }
     }
 
-    private void addPostParameter(char[] supportedParams, char paramKey
-            , List<HttpParameter> pagingParams, String paramName, long paramValue) {
-        boolean supported = false;
-        for (char supportedParam : supportedParams) {
+    private fun addPostParameter(
+        supportedParams: CharArray, paramKey: Char, pagingParams: MutableList<HttpParameter?>, paramName: String, paramValue: Long
+    ) {
+        var supported = false
+        for (supportedParam in supportedParams) {
             if (supportedParam == paramKey) {
-                supported = true;
-                break;
+                supported = true
+                break
             }
         }
-        if (!supported && -1 != paramValue) {
-            throw new IllegalStateException("Paging parameter [" + paramName
-                    + "] is not supported with this operation.");
+        check(!(!supported && -1L != paramValue)) {
+            ("Paging parameter [" + paramName
+                    + "] is not supported with this operation.")
         }
-        if (-1 != paramValue) {
-            pagingParams.add(new HttpParameter(paramName, String.valueOf(paramValue)));
+        if (-1L != paramValue) {
+            pagingParams.add(HttpParameter(paramName, paramValue.toString()))
         }
     }
 
-    public Paging() {
+    constructor()
+    constructor(page: Int) {
+        this.page = page
     }
 
-    public Paging(int page) {
-        setPage(page);
+    constructor(sinceId: Long) {
+        this.sinceId = sinceId
     }
 
-    public Paging(long sinceId) {
-        setSinceId(sinceId);
+    constructor(page: Int, count: Int) : this(page) {
+        this.count = count
     }
 
-    public Paging(int page, int count) {
-        this(page);
-        setCount(count);
+    constructor(page: Int, sinceId: Long) : this(page) {
+        this.sinceId = sinceId
     }
 
-    public Paging(int page, long sinceId) {
-        this(page);
-        setSinceId(sinceId);
+    constructor(page: Int, count: Int, sinceId: Long) : this(page, count) {
+        this.sinceId = sinceId
     }
 
-    public Paging(int page, int count, long sinceId) {
-        this(page, count);
-        setSinceId(sinceId);
+    constructor(page: Int, count: Int, sinceId: Long, maxId: Long) : this(page, count, sinceId) {
+        this.maxId = maxId
     }
 
-    public Paging(int page, int count, long sinceId, long maxId) {
-        this(page, count, sinceId);
-        setMaxId(maxId);
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Paging) return false
+        val paging = other
+        if (count != paging.count) return false
+        if (maxId != paging.maxId) return false
+        if (page != paging.page) return false
+        return if (sinceId != paging.sinceId) false else true
     }
 
-    public int getPage() {
-        return page;
+    override fun hashCode(): Int {
+        var result = page
+        result = 31 * result + count
+        result = 31 * result + (sinceId xor (sinceId ushr 32)).toInt()
+        result = 31 * result + (maxId xor (maxId ushr 32)).toInt()
+        return result
     }
 
-    public void setPage(int page) {
-        if (page < 1) {
-            throw new IllegalArgumentException("page should be positive integer. passed:" + page);
-        }
-        this.page = page;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        if (count < 1) {
-            throw new IllegalArgumentException("count should be positive integer. passed:" + count);
-        }
-        this.count = count;
-    }
-
-    public Paging count(int count) {
-        setCount(count);
-        return this;
-    }
-
-    public long getSinceId() {
-        return sinceId;
-    }
-
-    public void setSinceId(long sinceId) {
-        if (sinceId < 1) {
-            throw new IllegalArgumentException("since_id should be positive integer. passed:" + sinceId);
-        }
-        this.sinceId = sinceId;
-    }
-
-    public Paging sinceId(long sinceId) {
-        setSinceId(sinceId);
-        return this;
-    }
-
-    public long getMaxId() {
-        return maxId;
-    }
-
-    public void setMaxId(long maxId) {
-        if (maxId < 1) {
-            throw new IllegalArgumentException("max_id should be positive integer. passed:" + maxId);
-        }
-        this.maxId = maxId;
-    }
-
-    public Paging maxId(long maxId) {
-        setMaxId(maxId);
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Paging)) return false;
-
-        Paging paging = (Paging) o;
-
-        if (count != paging.count) return false;
-        if (maxId != paging.maxId) return false;
-        if (page != paging.page) return false;
-        if (sinceId != paging.sinceId) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = page;
-        result = 31 * result + count;
-        result = 31 * result + (int) (sinceId ^ (sinceId >>> 32));
-        result = 31 * result + (int) (maxId ^ (maxId >>> 32));
-        return result;
-    }
-
-    @Override
-    public String toString() {
+    override fun toString(): String {
         return "Paging{" +
                 "page=" + page +
                 ", count=" + count +
                 ", sinceId=" + sinceId +
                 ", maxId=" + maxId +
-                '}';
+                '}'
+    }
+
+    companion object {
+        private const val serialVersionUID = -7226113618341047983L
+
+        // since only
+        @JvmField
+        val S = charArrayOf('s')
+
+        // since, max_id, count, page
+        @JvmField
+        val SMCP = charArrayOf('s', 'm', 'c', 'p')
+        const val COUNT = "count"
+
+        // somewhat GET list statuses requires "per_page" instead of "count"
+        // @see <a href="https://dev.twitter.com/docs/api/1.1/get/:user/lists/:id/statuses">GET :user/lists/:id/statuses | Twitter Developers</a>
+        const val PER_PAGE = "per_page"
+
+        private val NULL_PARAMETER_ARRAY = arrayOfNulls<HttpParameter>(0)
+        private val NULL_PARAMETER_LIST: List<HttpParameter?> = ArrayList(0)
     }
 }
